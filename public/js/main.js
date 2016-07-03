525881566ae7394dcdb816babfa5fd0ad1e9938d
Ossama Hjaji <hjajiossama@gmail.com>
2016-07-04 00:16:28 +0200

    merge conflict 3

--- public/js/main.js
+++ public/js/main.js
@@ -10,48 +10,54 @@ angular.module('app', [])
 .service('DataService', ['$http', DataService])
 },{"./controllers/MainController":2,"./services/data":3,"angular":5}],2:[function(require,module,exports){
 module.exports = function($scope, DataService){
-//    $scope.newTodo = JSON.stringify{ title : '', description : ''};
-//    $scope.addNewTodo = function(){
-//        var config = {
-//            method: 'POST',
-//            url : 'http://104.236.68.81/api/tasks',
-//            data : {
-//                'title' : $scope.todo.title,
-//                'description' : $scope.todo.description
-//            }
-//        }
-//        var request = $http(config);
-//        request.then(function (response){
-//            console.log(response.data);
-//        },function(error){
-//            console.log(error.data);
-//        })
-//    }
+module.exports = function($scope, $http, DataService){
+    // $scope.newTodo = JSON.stringify{ title : '', description : ''};
+    $scope.addNewTodo = function(){
+        var config = {
+            method: 'POST',
+            url : 'http://104.236.68.81/api/tasks',
+            data : {
+                'title' : $scope.todo.title,
+                'description' : $scope.todo.description
+            }
+        }
+        var request = $http(config);
+        request.then(function (response){
+            console.log(response.data);
+        },function(error){
+            console.log(error.data);
+        })
+    }
 
     DataService.getTodos(function(response){
         console.log(response.data);
         $scope.todos = response.data;
     })
-    //DataService.addTodo();
-    $scope.completeTodo = function(index){
-      console.log(index);
+
+    $scope.saveTodos = function(){
+        var filteredTodos = $scope.todos.filter(function(todo){
+            if(todo.edited){
+                return todo;
+            }
+        })
+        DataService.saveTodos(filteredTodos);
     }
 }
 
 },{}],3:[function(require,module,exports){
 module.exports = function($http, $scope){
-   
+
     this.getTodos = function(callback){
         $http.get('http://104.236.68.81/api/tasks')
         .then(callback)
     }
-    
-    
+
+
 //    this.addTodos = function(todo){
-//        
+//
 //        $http.post('http://104.236.68.81/api/tasks', todo)
-//        
-//        
+//
+//
 //        .then(console.log(todo.length + 'have been saved.'))
 //    }
 }
