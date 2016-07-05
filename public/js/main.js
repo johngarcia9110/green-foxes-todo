9b9559c41a30f80467178512e2a615d1e8627673
Ossama Hjaji <hjajiossama@gmail.com>
2016-07-05 23:31:23 +0200

    Color Markup to reflect the level of priority for each task

--- public/js/main.js
+++ public/js/main.js
@@ -28,6 +28,32 @@ module.exports = function($scope, DataService){
 //        })
 //    }
 
+$scope.getStyle = function (todo) {
+  if (todo.completed) {
+    var textDecoration = 'line-through';
+    var color = '#D2D2D2';
+  } else {
+    var textDecoration = 'none';
+    var color = '#000000';
+  }
+var borderColor
+  switch(todo.priority) {
+    case 0:
+        borderColor = '#45C67D'
+        break;
+    case 1:
+        borderColor = '#FF7700'
+        break;
+    case 2:
+        borderColor = '#FF0000'
+}
+    return {
+         'text-decoration': textDecoration,
+         'color': color,
+         'border-color': borderColor
+    }
+};
+
     DataService.getTodos(function(response){
         console.log(response.data);
         $scope.todos = response.data;
