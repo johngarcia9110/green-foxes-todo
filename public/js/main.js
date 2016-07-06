878aed87e03299dcae87aa3b5de856083d7d7876
John B Garcia <johngarcia9110@gmail.com>
2016-07-06 16:15:01 -0400

    Merge Conflict

--- public/js/main.js
+++ public/js/main.js
@@ -28,22 +28,41 @@ module.exports = function($scope, DataService){
 //        })
 //    }
 
-    DataService.getTodos(function(response){
-        console.log(response.data);
-        $scope.todos = response.data;
-    })
-    
-    DataService.deleteTodo(function(todo){
-        
-    })
-    
-    
-//    //DataService.addTodo();
-//    $scope.completeTodo = (function(index){
-//      console.log(index);
-//    })
-}
+	$scope.getStyle = function (todo) {
+	  if (todo.completed) {
+		var textDecoration = 'line-through';
+		var color = '#D2D2D2';
+	  } else {
+		var textDecoration = 'none';
+		var color = '#000000';
+	  }
+	var borderColor
+	  switch(todo.priority) {
+		case 0:
+			borderColor = '#45C67D'
+			break;
+		case 1:
+			borderColor = '#FF7700'
+			break;
+		case 2:
+			borderColor = '#FF0000'
+	}
+		return {
+			 'text-decoration': textDecoration,
+			 'color': color,
+			 'border-color': borderColor
+		}
+	};
 
+		DataService.getTodos(function(response){
+			console.log(response.data);
+			$scope.todos = response.data;
+		})
+		//DataService.addTodo();
+		$scope.completeTodo = function(index){
+		  console.log(index);
+		}
+}
 },{}],3:[function(require,module,exports){
 module.exports = function($http, $scope){
    
@@ -52,9 +71,6 @@ module.exports = function($http, $scope){
         .then(callback)
     }
     
-    this.deleteTodo = function(todo){
-        $http.delete('http://104.236.68.81/api/tasks/' + todo);
-    }
     
 //    this.addTodos = function(todo){
 //        
